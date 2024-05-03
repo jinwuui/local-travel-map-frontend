a757bd9d7fd99a926a6b7c4be897a409dba35846
jinwuui <jinwuui@gmail.com>
2024-05-03 17:00:14 +0900

    feat: Add google map

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <GoogleMap id="map" :api-key="apiKey" :center="center" :zoom="3">
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
+
+<script setup>
+import { GoogleMap, Marker } from "vue3-google-map";
+
+const center = { lat: 40.689247, lng: -74.044502 };
+const apiKey = process.env.VUE_APP_MAP_KEY;
+</script>
+
+<style>
+#map {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
