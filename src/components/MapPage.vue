bbea774eebfd7f05d321bfc0e2a40e0458a49be1
박진우 <jinwoo@bagjin-uui-noteubug.local>
2024-06-11 10:46:47 +0900

    chore: change google map design

--- src/components/MapPage.vue
+++ src/components/MapPage.vue
@@ -7,6 +7,8 @@
       :center="mapCenter"
       :zoom="mapZoom"
       draggableCursor="default"
+      mapTypeId="terrain"
+      :styles="styles"
       region="KR"
       language="ko"
       :fullscreenControl="screenControl.fullscreenControl"
@@ -51,9 +53,108 @@ const screenControl = {
   fullscreenControl: false,
   scaleControl: false,
   zoomControl: false,
-  mapTypeControl: true,
   mapTypeControlOptions: { position: 7 },
 };
+
+const styles = [
+  {
+    featureType: "landscape",
+    stylers: [
+      {
+        hue: "#FFA800",
+      },
+      {
+        saturation: 0,
+      },
+      {
+        lightness: 0,
+      },
+      {
+        gamma: 1,
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    stylers: [
+      {
+        hue: "#53FF00",
+      },
+      {
+        saturation: -73,
+      },
+      {
+        lightness: 40,
+      },
+      {
+        gamma: 1,
+      },
+    ],
+  },
+  {
+    featureType: "road.arterial",
+    stylers: [
+      {
+        hue: "#FBFF00",
+      },
+      {
+        saturation: 0,
+      },
+      {
+        lightness: 0,
+      },
+      {
+        gamma: 1,
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    stylers: [
+      {
+        hue: "#00FFFD",
+      },
+      {
+        saturation: 0,
+      },
+      {
+        lightness: 30,
+      },
+      {
+        gamma: 1,
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#90bdca",
+      },
+      {
+        lightness: "10",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    stylers: [
+      {
+        hue: "#679714",
+      },
+      {
+        saturation: 33.4,
+      },
+      {
+        lightness: -25.4,
+      },
+      {
+        gamma: 1,
+      },
+    ],
+  },
+];
 
 onMounted(async () => await fetchPlaces());
 
