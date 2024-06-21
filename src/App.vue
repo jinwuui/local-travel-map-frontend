636564c196bd4899e693209195be116d2a3942e0
jinwuui <jinwuui@gmail.com>
2024-06-22 06:54:57 +0900

    feat: add mobile warning page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <MapPage />
-    <AppHeader />
-    <AppNavBar />
-    <AppSideTab />
+    <div class="mobile-warning">
+      <h1>모바일 버전은<br />곧 공개됩니다</h1>
+      <h2>데스크탑 환경에서 여행지를 만나보세요.</h2>
+    </div>
+    <div class="main-content">
+      <MapPage />
+      <AppHeader />
+      <AppNavBar />
+      <AppSideTab />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { onBeforeMount } from "vue";
+import { onMounted, ref, onBeforeMount } from "vue";
 
 import MapPage from "@/components/MapPage.vue";
 import AppHeader from "@/components/header/AppHeader.vue";
@@ -17,6 +23,18 @@ import AppSideTab from "@/components/body/AppSideTab.vue";
 
 onBeforeMount(() => {
   document.title = "로-컬 | 숨은 여행지 찾기";
+});
+
+const isMobile = ref(false);
+
+const checkMobile = () => {
+  isMobile.value = window.innerWidth <= 768;
+  console.log(window.innerWidth);
+};
+
+onMounted(() => {
+  checkMobile();
+  window.addEventListener("resize", checkMobile);
 });
 </script>
 
@@ -49,5 +67,33 @@ onBeforeMount(() => {
     Helvetica, Arial, sans-serif;
   width: 100%;
   height: 100%;
+}
+
+.mobile-warning {
+  display: none;
+}
+
+.main-content {
+  display: block;
+}
+
+@media (max-width: 768px) {
+  .mobile-warning {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100vh;
+    padding: 20px;
+    background-color: #f8d7da;
+    color: #721c24;
+    font-family: "DOSIyagiBoldface", "DungGeunMo", "DNFBitBitv2", Avenir,
+      Helvetica, Arial, sans-serif;
+  }
+
+  .main-content {
+    display: none;
+  }
 }
 </style>
